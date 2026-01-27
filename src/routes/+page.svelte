5c5a2781ac53bdfb86c483a66fe88c1c936880a7
Lucas Lange <lucas@langes.us>
2026-01-27 12:19:06 -0500

    fix vite processing link issue

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,7 @@
     content: [
       `If you have suggestions for a project or would like to contact me for
       another reason, feel free to email me at <a
-        href=&quot;mailto:lucas@langes.us&quot;>lucas@langes.us</a
+        href=&quot;mailto:lucas@langes.us&quot; vite-ignore>lucas@langes.us</a
       >. I'm always open to comments, questions, and suggestions.
       Alternately, you can find me on linkedin, github, and discord using
       the links below.`,
