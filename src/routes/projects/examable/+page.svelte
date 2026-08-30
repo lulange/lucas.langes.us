d5fe9dc572aa82533189992c8b48ceadbbf09c1c
Lucas Lange <lucas@langes.us>
2026-08-30 17:34:13 -0400

    add new pages for limelight and examable. TODOs for content is still present

---
+++ src/routes/projects/examable/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+    import Banner from "$lib/components/banner.svelte";
+    import TextSection from "$lib/components/textSection.svelte";
+    import { onMount } from "svelte";
+    import { addSquares } from '$lib/js/squares.js';
+    import FormattedImage from "$lib/components/formattedImage.svelte";
+    
+    onMount(() => {
+      const boxStartRight = document.getElementById("boxes-start-right");
+      const boxStartLeft = document.getElementById("boxes-start-left");
+      const boxEnd = document.getElementById("boxes-end");
+      
+      setTimeout(function() {addSquares(boxStartLeft, boxStartRight, boxEnd);}, 500);
+      
+    });
+    
+    const pageContent = [
+      `TODO describe examable`,
+      `<a class="lead" target="_blank" href="https://github.com/lulange/examable">Check out the Github page!</a>`,
+    ];
+  </script>
+  
+  <div id="boxes-start-right"></div>
+  <div id="boxes-start-left"></div>
+  
+  <Banner heading="Examable"/>
+  
+  <FormattedImage 
+    source="/images/ExamableScreenshot.png"
+    altText="A dashboard welcoming the user and providing both usage statistics and options to navigate."
+    explainerText="Examable's dashboard."
+  />
+  
+  <TextSection
+    content={pageContent}
+  />
+  
+  <div id="boxes-end"></div>
---
+++ src/routes/projects/limeLight/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+    import Banner from "$lib/components/banner.svelte";
+    import TextSection from "$lib/components/textSection.svelte";
+    import { onMount } from "svelte";
+    import { addSquares } from '$lib/js/squares.js';
+    import FormattedImage from "$lib/components/formattedImage.svelte";
+    
+    onMount(() => {
+      const boxStartRight = document.getElementById("boxes-start-right");
+      const boxStartLeft = document.getElementById("boxes-start-left");
+      const boxEnd = document.getElementById("boxes-end");
+      
+      setTimeout(function() {addSquares(boxStartLeft, boxStartRight, boxEnd);}, 500);
+      
+    });
+    
+    const pageContent = [
+      `TODO.`,
+      `<a class="lead" target="_blank" href="https://github.com/lulange/LimeLight">Check out the Github page!</a>`,
+    ];
+  </script>
+  
+  <div id="boxes-start-right"></div>
+  <div id="boxes-start-left"></div>
+  
+  <Banner heading="LimeLight"/>
+  
+  <FormattedImage 
+    source="/images/LimeLightScreenshot.png"
+    altText="A dark webpage with a title saying 'the premier platform for startup investing'."
+    explainerText="LimeLight's home page."
+  />
+  
+  <TextSection
+    content={pageContent}
+  />
+  
+  <div id="boxes-end"></div>
