a1af29dab06bcbb2d95da6a99b30cfbdc8073364
Lucas Lange <lucas@langes.us>
2026-09-05 14:54:41 -0400

    add a 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,29 @@
+<script>
+  import TextSection from "$lib/components/textSection.svelte";
+  import Banner from "$lib/components/banner.svelte";
+  import { onMount } from "svelte";
+  import { addSquares } from '$lib/js/squares.js';
+  
+  onMount(() => {
+    const boxStartRight = document.getElementById("boxes-start-right");
+    const boxStartLeft = document.getElementById("boxes-start-left");
+    const boxEnd = document.getElementById("boxes-end");
+    
+    setTimeout(function() {addSquares(boxStartLeft, boxStartRight, boxEnd);}, 500); 
+  });
+  
+  const notFound = [
+    `Unfortunately, the page you are looking for does not exist.`
+  ];
+</script>
+
+<div id="boxes-start-right"></div>
+<div id="boxes-start-left"></div>
+
+<Banner heading="Page Not Found" />
+
+<TextSection
+  content={notFound}
+/>
+
+<div id="boxes-end"></div>
